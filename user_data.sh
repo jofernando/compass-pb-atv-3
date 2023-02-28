4eaa837fdc6e0138ae405f65954bd5a68492067e
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-02-28 14:12:26 -0300

    arquivo contendo user data

---
+++ user_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum update --assumeyes
+yum install --assumeyes docker
+systemctl enable --now docker
+
+DOCKER_CONFIG=/usr/local/lib/docker
+mkdir --parents $DOCKER_CONFIG/cli-plugins
+curl --location https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 --output $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+usermod --append --groups docker ec2-user
+
+cd /root/
+git clone https://github.com/jofernando/compass-pb-atv-3.git
+cd compass-pb-atv-3
+
+mkdir --parents /efs/
+cat etc/fstab >> /etc/fstab
+
+mv etc/profile.d/00-aliases.sh /etc/profile.d/00-aliases.sh
+
+docker compose up --detach
